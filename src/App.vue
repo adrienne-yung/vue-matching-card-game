2a19afc349be203b464fbc3dafbe96d9a426e6bc
Adrienne Yung <adrienneyung64@gmail.com>
2022-06-10 15:06:20 -0700

    Created card component with a value prop.

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>Summer Vue</h1>
+    <section class="game-board">
+    <Card v-for="(card, index) in cardList"
+    :key="`card-${index}`"
+    :value="card"
+    />
+    </section>
+</template>
+
+<script>
+import Card from './components/Card-File'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  setup() {
+    const cardList = []
+
+    for (let i = 0; i< 16; i++) {
+      cardList.push(i)
+    }
+    return {
+      cardList
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.card {
+  border: 5px solid rgb(171,219,227);
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Card-File.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="card">
+  {{ value }}
+  </div>
+</template>
